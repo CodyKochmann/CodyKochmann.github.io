386b6474d36fbbaed8e23fc27de0e0096ab11625
Cody L. Kochmann <kochmanncody@gmail.com>
2015-05-13 00:10:33 -0700

    added header.json for testing and a main_container for the body

--- js/header.js
+++ js/header.js
@@ -1,18 +1,30 @@
 var header_html = '<div id="website_header"><p id="name">Cody Kochmann</p><p id="projects_button">Projects</p><p id="snippets_button">Snippets</p><p id="resume_button">Resume</p></div>';
 $('document').ready(function() {
     $('body').append(header_html);
-    var projects = $("#projects_button");
-    var snippets = $("#snippets_button");
-    var resume = $("#resume_button");
 
-    projects.click(function(event) {
+    /*$("#projects_button").click(function(event) {
         window.location = "http://cody.pw/projects/";
     });
-    snippets.click((function(){
+    $("#snippets_button").click((function(){
         window.location = "http://cody.pw/snippets/"; 
     });
-    resume.click((function(){
+    $("#resume_button").click((function(){
         window.location = "http://cody.pw/resume/"; 
-    });
+    });*/
+    
+$.getJSON( "http://cody.pw/json/header.json", function( data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  });
+ 
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
+});
     
 });
+
+
+
