11556b9b525d54661de88acce2aef4a009ec1457
Cody L. Kochmann <kochmanncody@gmail.com>
2015-05-13 00:00:46 -0700

    working on website header

--- js/header.js
+++ js/header.js
@@ -1,4 +1,18 @@
+var header_html = '<div id="website_header"><p id="name">Cody Kochmann</p><p id="projects_button">Projects</p><p id="snippets_button">Snippets</p><p id="resume_button">Resume</p></div>';
 $('document').ready(function() {
-    var header = $("#website_header");
+    $('body').append(header_html);
+    var projects = $("#projects_button");
+    var snippets = $("#snippets_button");
+    var resume = $("#resume_button");
 
+    projects.click(function(event) {
+        window.location = "http://cody.pw/projects/";
+    });
+    snippets.click((function(){
+        window.location = "http://cody.pw/snippets/"; 
+    });
+    resume.click((function(){
+        window.location = "http://cody.pw/resume/"; 
+    });
+    
 });
