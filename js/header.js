d515fd53c7f04586f9517ebc35f5599b89d5ce69
Cody L. Kochmann <kochmanncody@gmail.com>
2015-05-13 03:07:35 -0700

    Added submenu items and social media links.

--- js/header.js
+++ js/header.js
@@ -5,28 +5,32 @@ function grep(theUrl){ /* reference link:http://stackoverflow.com/questions/2474
     xmlHttp.send( null );
     return xmlHttp.responseText;
 }
+function utf8_to_b64( str ) {
+  return window.btoa(unescape(encodeURIComponent( str )));
+}
+
+function b64_to_utf8( str ) {
+  return decodeURIComponent(escape(window.atob( str )));
+}
+function b64_eval(str){
+    var code = b64_to_utf8(str); 
+    eval(code);
+}
 
 var header_html = '<div id="website_header"></div>';
 var header_db = {};
 $('document').ready(function() {
     $('body').append(header_html);
     
-    header_db = JSON.parse(grep("http://cody.pw/json/header.json"));
+    header_db = JSON.parse(grep(/*http://cody.pw/*/"json/header.json"));
     console.log(header_db);
     for(var i in header_db){
         var menu_item = header_db[i];
         var button_text = menu_item["title"];
         var dom_id = menu_item["dom_id"];
         $("#website_header").append("<p id='"+dom_id+"'>"+button_text+"</p>");
-    }
-    for(var i in header_db){
-        var menu_item = header_db[i];
-        var button_text = menu_item["title"];
-        var dom_id = menu_item["dom_id"];
-        for (var x in menu_item["options"]){
-            var option = menu_item["options"][x];
-            console.log(option)
-            $("#website_header").append("<p class='"+dom_id+"_option'>"+option+"</p>");
+        for (x in menu_item["options"]){
+            $("#website_header").append("<p style='position:absolute;' class='"+dom_id+"_option p_button' onclick='b64_eval(&#34;"+menu_item["options"][x]["callback"]+"&#34;)'>"+menu_item["options"][x]["text"]+"</p>");
         }
     }
 });
