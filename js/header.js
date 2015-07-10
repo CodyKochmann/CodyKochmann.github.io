ef3f7ea39a932bebd0e9dc58e5129bdb4fb5898a
Cody Kochmann <kochmanncody@gmail.com>
2015-07-10 11:03:31 -0700

    Made the json call safer for debugging.
    I needed the full link to be called for the header.json file because this script is being injected into other webpages that are not included in this website when I am prototyping ideas.

--- js/header.js
+++ js/header.js
@@ -13,7 +13,7 @@ var inject_css = function(a) {
     document.getElementsByTagName("head")[0].appendChild(b)
 }
 
-inject_css(grep("css/header.css"));
+inject_css(grep("http://cody.pw/css/header.css"));
 //inject_css(grep("css/bug_reporter.css"));
 
 var b64=function(){
@@ -29,7 +29,7 @@ var header_db = {};
 $('document').ready(function() {
     $('body').append('<div id="website_header"></div>');
 
-    header_db = JSON.parse(grep( /*http://cody.pw/*/ "json/header.json"));
+    header_db = JSON.parse(grep("http://cody.pw/json/header.json"));
     console.log(header_db);
     for (var i in header_db) {
         var menu_item = header_db[i];
