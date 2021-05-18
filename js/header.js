94f1460b9458d9da0716e3d2d3125b50ffae51fa
Cody Kochmann <kochmanncody@gmail.com>
2021-05-18 17:54:52 -0600

    autocommit

--- js/header.js
+++ js/header.js
@@ -36,7 +36,7 @@ var header_db = {};
 $('document').ready(function() {
     $('body').append('<div id="website_header"></div>');
 
-    header_db = JSON.parse(grep("http://mo-powah.baby/json/header.json"));
+    header_db = JSON.parse(grep("/json/header.json"));
     console.log(header_db);
     for (var i in header_db) {
         var menu_item = header_db[i];
